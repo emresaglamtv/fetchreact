81967de5531fd9b61bca2c4a7965e0274cdd23f6
Emre SAGLAM <69041945+cukulaci@users.noreply.github.com>
2023-10-01 13:17:26 +0300

    Axios | React

--- src/component/users.js
+++ src/component/users.js
@@ -1,13 +1,13 @@
 import {useEffect, useState} from 'react'
+import axios from 'axios';
 
 function Users() {
     const [users, setUsers] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/users")
-        .then((response) => response.json())
-        .then((data) => setUsers(data))
+        axios("https://jsonplaceholder.typicode.com/users")
+        .then((response) => setUsers(response.data))
         .catch((event) => console.log(event))
         .finally(() => setIsLoading(false))
     },[])
